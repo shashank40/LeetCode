cd8e75948804acd6fd1acf596f46368a830d0035
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-01-10 18:13:09 +0530

    Time: 2 ms (13.96%), Space: 39.3 MB (51.91%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void nextPermutation(int[] arr) {
+        int n=arr.length;
+        
+        int temp=0;
+    int max=Integer.MIN_VALUE;
+    int part=-1;
+        for(int i=n-1;i>0;i--){
+            temp=arr[i];
+            for(int j=i-1;j>=0;j--){
+                    if(arr[j]<temp){
+                       if(j>max){
+                            max=j;
+                            part=i;
+                        } 
+                    }
+            }
+        }
+
+       if(part==-1){
+        for(int i=0;i<n/2;i++)
+        {
+            part=arr[i];
+            arr[i]=arr[n-1-i];
+            arr[n-1-i]=part;
+        }
+        return;}
+
+        temp=arr[part];
+        arr[part]=arr[max];
+        arr[max]=temp;
+
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+        for(int i=max+1;i<n;i++){
+            pq.add(arr[i]);
+        }
+
+        for(int i=max+1;i<n;i++){
+            arr[i]=pq.poll();
+        }
+    }
+}
