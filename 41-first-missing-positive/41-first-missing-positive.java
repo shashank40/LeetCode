0180793f8c9364bd40a6c8d7a0966ff254347c42
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-01-25 17:29:57 +0530

    Time: 50 ms (12.22%), Space: 159.1 MB (8.16%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,24 +1,32 @@
 class Solution {
+    boolean val[];
     public int firstMissingPositive(int[] A) {
          int n=A.length;
-        HashSet<Integer> s=new HashSet<>();
+       val=new boolean[500001];
         int max=Integer.MIN_VALUE;
-
+        int min=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            if(A[i]>0)
-            s.add(A[i]);
-
+            
+                if(A[i]>0)
+                    min=Math.min(min,A[i]);
+            
             max=Math.max(max,A[i]);
         }
 
-        if(max<=0)
+        if(max<=0 || min>1)
         return 1;
-
-        for(int i=1;i<=max;i++){
-            if(!s.contains(i))
-            return i;
+        
+        for(int i=0;i<n;i++){
+            if(A[i]>0 && (A[i]-min)<500001)
+                val[A[i]-min]=true;
         }
-
+        
+        for(int i=0;i<500001;i++)
+            if(val[i]==false)
+                return (i+min);
+        
+        
         return (max+1);
+        
     }
 }
