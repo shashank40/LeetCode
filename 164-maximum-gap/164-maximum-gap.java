b5c8b123bf3b4c2e9d6742db0514373d5c14a89b
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-01-31 00:34:43 +0530

    Time: 58 ms (35.84%), Space: 82.6 MB (48.17%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.java
+++ 164-maximum-gap/164-maximum-gap.java
@@ -1,11 +1,8 @@
 class Solution {
-    public int maximumGap(int[] nums) {
-        int n=nums.length;
+    public int maximumGap(int[] arr) {
+        int n=arr.length;
         if(n<=1)
         return 0;
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++)
-        arr[i]=nums[i];
         Arrays.sort(arr);
         int max=Integer.MIN_VALUE;
         for(int i=1;i<n;i++)
