525848180433c1c3731fb63b4caf53c7045907cb
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-02-01 18:44:51 +0530

    Time: 18 ms (81.67%), Space: 52.3 MB (81.43%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.java
+++ 164-maximum-gap/164-maximum-gap.java
@@ -3,12 +3,36 @@ class Solution {
         int n=arr.length;
         if(n<=1)
         return 0;
-        Arrays.sort(arr);
-        int max=Integer.MIN_VALUE;
-        for(int i=1;i<n;i++)
-        max=Math.max(max,arr[i]-arr[i-1]);
-
-        return max;
         
+       int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,arr[i]);
+            min=Math.min(min,arr[i]);
+        }
+        
+        double size=(double)(max-min)/(double)(n);
+        int mx[]=new int[n];
+        int mn[]=new int[n];
+        Arrays.fill(mn,Integer.MAX_VALUE);
+        Arrays.fill(mx,Integer.MIN_VALUE);
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]==min || arr[i]==max) continue;
+            int index=(arr[i]-min)/(int)Math.ceil(size);
+            mx[index]=Math.max(mx[index],arr[i]);
+            mn[index]=Math.min(mn[index],arr[i]);
+        }
+        int ans=0;
+        int prev=min;
+        for(int i=0;i<n;i++){
+            if(mn[i]==Integer.MAX_VALUE)continue;
+            ans=Math.max(ans,mn[i]-prev);
+        prev=mx[i];}
+        
+        
+            ans=Math.max(ans,max-prev);
+        
+        return ans;
     }
 }
