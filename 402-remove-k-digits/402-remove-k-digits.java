97b1f998559367e045b0eb72f917371ff65e599b
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-02-18 19:23:43 +0530

    Time: 30 ms (22.08%), Space: 42.4 MB (56.57%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.java
+++ 402-remove-k-digits/402-remove-k-digits.java
@@ -1,40 +1,45 @@
 class Solution {
     public String removeKdigits(String num, int k) {
         int n=num.length();
-        if(n-k==0||num.equals("0"))
-            return "0";
-        char c[]=num.toCharArray();
-        char prev=' ';
-        String ans="";
-        char arr[]=new char[n-k];
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(j<(n-k)&&(j==0||arr[j-1]<c[i])){
-                arr[j]=c[i];
-                j++;
-                continue;
-            }
-            else{
-            while(j>0 && arr[j-1]>c[i] && (n-k-j)<=(n-1-i)){
-                j--;
-            }   
-                if(j<n-k){
-                arr[j]=c[i];
-                j++; }
-            }
-            
-        }
-        String ok="";
-        boolean fun=false;
-        for(char m:arr){
-            if(m=='0'&&!fun)
-                continue;
-            fun=true;
-            ok+=m;
-        }
-        if(ok.length()==0)
+        if(n<=k)
             return "0";
         
-        return ok;
+        int m=n-k;
+        Stack<Character> s=new Stack<>();
+        for(int i=0;i<n;i++){
+             char c=num.charAt(i);
+            if(s.size()<m && (s.size()==0||c>=s.peek())){
+                s.push(c);
+                continue;
+                }
+            if(c>=s.peek())
+                continue;
+            
+            while(s.size()>0 && (n-1-i)>=(m-s.size()) && c<s.peek())
+                s.pop();
+            
+            s.push(c);
+        }
+               
+        if(s.size()==0)
+            return "0";
+        
+        String ans="";
+        
+        while(s.size()>0)
+            ans=s.pop()+ans;
+        
+        int z=ans.length();
+        int i=0;
+        
+        while(i<z && ans.charAt(i)=='0'){
+            i++;
+        }
+        
+        ans=ans.substring(i);
+        if(ans.length()==0)
+            return "0";
+        
+        return ans;
     }
 }
