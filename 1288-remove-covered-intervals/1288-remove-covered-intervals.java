fbc6599b70b6e1c3802c14fd634f1469538774cd
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-02-20 21:43:41 +0530

    Time: 4 ms (98.13%), Space: 42.3 MB (72.54%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,41 +1,56 @@
 class Solution {
     public int removeCoveredIntervals(int[][] s) {
         int n=s.length;
-        pair p[]=new pair[n];
+        Arrays.sort(s,new cmp());
+//         pair p[]=new pair[n];
         
-        for(int i=0;i<n;i++){
-            p[i]=new pair(s[i][0],s[i][1]);
-        }
+//         for(int i=0;i<n;i++){
+//             p[i]=new pair(s[i][0],s[i][1]);
+//         }
         
-        Arrays.sort(p);
+//         Arrays.sort(p);
         int count=1;
-        int j=p[0].b;
+        int j=s[0][1];
         
+        // for(int i=1;i<n;i++){
+        //     if(p[i].b>j){
+        //         count++;
+        //         j=p[i].b;
+        //     }
+        // }
         for(int i=1;i<n;i++){
-            if(p[i].b>j){
+            if(s[i][1]>j){
                 count++;
-                j=p[i].b;
+                j=s[i][1];
             }
         }
-        
         return count;
     }
 }
 
-class pair implements Comparable<pair>
-{
-    int a;
-    int b;
+// class pair implements Comparable<pair>
+// {
+//     int a;
+//     int b;
     
-    pair(int i, int j){
-    a=i;
-    b=j;
+//     pair(int i, int j){
+//     a=i;
+//     b=j;
+//     }
+    
+//     public int compareTo(pair p){
+//     if(this.a-p.a==0)
+//     return p.b-this.b;
+    
+//     return this.a-p.a;
+//     }
+// }
+class cmp implements Comparator<int[]>{ ////////// <HERE WE PUT NON PRIMITIVE TYPES, so int[] is non primitive too>
+    public int compare(int a[], int b[])
+    {
+        if(a[0]-b[0]==0)
+        return b[1]-a[1];
+        
+        return a[0]-b[0];
     }
-    
-    public int compareTo(pair p){
-    if(this.a-p.a==0)
-    return p.b-this.b;
-    
-    return this.a-p.a;
     }
-}
