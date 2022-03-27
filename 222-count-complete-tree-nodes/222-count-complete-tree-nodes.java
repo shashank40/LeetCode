d8f1edbabc90b2b3178c3be8aa6569525a620f9f
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-03-27 17:34:55 +0530

    Time: 0 ms (100.00%), Space: 49.6 MB (67.58%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -18,6 +18,30 @@ class Solution {
         if(root==null)
             return 0;
         
-        return countNodes(root.left)+countNodes(root.right)+1;
+        int left=left(root.left);
+        int right=right(root.right);
+        if(right==left)
+            return (int)Math.pow(2,left+1)-1;
+        
+        return calc(root);
+       
     }
+    
+    public int left(TreeNode root){
+        if(root==null)
+            return 0;
+        
+        return 1+left(root.left);
+    }
+    public int right(TreeNode root){
+        if(root==null)
+            return 0;
+        
+        return 1+right(root.right);
+    }
+public int calc(TreeNode root){
+    if(root==null)
+            return 0;
+     return calc(root.left)+calc(root.right)+1;
+}
 }
