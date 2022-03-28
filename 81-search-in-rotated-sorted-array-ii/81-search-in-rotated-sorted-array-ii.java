039e99c1fbdb912c5de0d733d9ab9b7741afc2e8
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-03-29 00:16:21 +0530

    Time: 1 ms (81.31%), Space: 43.8 MB (45.64%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,32 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-       for(int a:nums)
-           if(a==target)
-               return true;
+        ///Logic is focus on sorted part, if not in that then move to another.
+        int l=0;
+        int r=nums.length-1;
         
+        while(l<=r){
+                
+            int m=l+(r-l)/2;
+            
+            if(nums[m]==target)
+                return true;
+            else if(nums[m]>nums[l]){
+                if(nums[m]>target && nums[l]<=target)
+                    r=m-1;
+                else
+                    l=m+1;
+                    
+            }
+            
+            else if(nums[m]<nums[l]){
+                if(nums[m]<target && nums[r]>=target)
+                    l=m+1;
+                else
+                    r=m-1;
+            }
+            else
+                l++;
+        }
         
         return false;
     }
