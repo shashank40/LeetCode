6db83989bdf6cf96a896643006c52fe93a87f8d6
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-03-29 21:30:45 +0530

    Time: 2 ms (12.32%), Space: 43.8 MB (46.28%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         ///Logic is focus on sorted part, if not in that then move to another.
+        //// worst case becomes linear as jb duplicates will have to pass them linearly
+        /// ex 10111 , target 0
+        /// if l and mid wala same to dk where to go, to l++ ki next wale se check kro ki ye sorted part hei ya duplocate
+        //// if l wala < m means left sorted then do jaise krte hein
+        ////   
         int l=0;
         int r=nums.length-1;
         
@@ -15,7 +20,7 @@ class Solution {
                     r=m-1;
                 else
                     l=m+1;
-                    
+                     //// only viewing from left k perspective ki if l wala and mid wala same , dont know where to go, to try to do l++ to have new check.
             }
             
             else if(nums[m]<nums[l]){
