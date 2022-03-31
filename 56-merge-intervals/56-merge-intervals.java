0aa1a97106fc4ff9402b7e6566a36c7c83d89870
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-03-31 17:37:35 +0530

    Time: 14 ms (46.06%), Space: 55.4 MB (42.55%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -37,9 +37,7 @@ class Solution {
 
 class cmp implements Comparator<int[]>{
     public int compare(int[] a, int[] b){
-        if(a[0]-b[0]!=0)
-            return a[0]-b[0];
         
-        return a[1]-b[1];
+        return a[0]-b[0];
     }
 }
