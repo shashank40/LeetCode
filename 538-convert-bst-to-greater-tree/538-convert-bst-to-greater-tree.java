db91fc0b6495ee84ae28bb5e2f1833c81b7788df
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-04-19 17:43:33 +0530

    Time: 6 ms (5.81%), Space: 53.8 MB (6.17%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -15,39 +15,40 @@
  */
 class Solution {
     public TreeNode convertBST(TreeNode root) {
-        if(root==null ||(root.left==null && root.right==null))
+        if(root==null || (root.left==null && root.right==null))
             return root;
-        Queue<TreeNode> q=new LinkedList<>();
-        ArrayList<Integer> arr=new ArrayList<>();
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode temp=q.poll();
-            arr.add(temp.val);
-            if(temp.left!=null)
-                q.add(temp.left);
-            if(temp.right!=null)
-                q.add(temp.right);
-        }
         
-        Collections.sort(arr);
-        int prev=0;
-        int n=arr.size();
-        for(int i=n-1;i>=0;i--){
-            mp.put(arr.get(i),prev+arr.get(i));
-            prev+=arr.get(i);
-        }
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode temp=q.poll();
-            temp.val=mp.get(temp.val);
-            if(temp.left!=null)
-                q.add(temp.left);
-            if(temp.right!=null)
-                q.add(temp.right);
-        }
+        set(root,0);
         
         return root;
+    }
+    public void set(TreeNode root,int prev){
+        if(root==null)
+            return;
         
+        int right=sum(root.right);
+        root.val+=(right+prev);
+        set(root.left,root.val);
+        set(root.right,prev);
+    }
+    
+    public int sum(TreeNode root){
+        if(root==null)
+            return 0;
+        
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        int sum=0;
+        while(!q.isEmpty()){
+            TreeNode temp=q.poll();
+            sum+=temp.val;
+            if(temp.left!=null)
+                q.add(temp.left);
+            if(temp.right!=null)
+                q.add(temp.right);
+            
+        }
+        
+        return sum;
     }
 }
