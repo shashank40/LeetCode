a070e1bcc12ec104c3d4f8058c0c5cf163c2f280
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-09-17 02:58:52 +0530

    Time: 318 ms (46.34%), Space: 264.7 MB (7.96%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,17 +1,16 @@
 class Solution {
     Integer t[][]=new Integer[1000][1000];
     public int maximumScore(int[] nums, int[] mul) {
-        return value(0,0,nums,mul);
+        return value(0,nums.length,0,nums,mul);
     }
     
-    public int value(int x,int i, int nums[], int mul[]){
+    public int value(int x,int n,int i, int nums[], int mul[]){
         if(i==mul.length)
             return 0;
         if(t[x][i]!=null)
             return t[x][i];
-        int val = nums[x]*mul[i] + value(x+1,i+1,nums,mul);
-        int val2= nums[nums.length-1-i+x]*mul[i] + value(x,i+1,nums,mul);
  
-        return t[x][i]= Math.max(val,val2);
+        return t[x][i]= Math.max(nums[x]*mul[i] + value(x+1,n,i+1,nums,mul),
+                                 nums[n-1-i+x]*mul[i] + value(x,n,i+1,nums,mul));
     }
 }
