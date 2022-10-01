bab4ab41821dadf997f666560c7028af3803a892
Shashank Tiwari <f20180630@pilani.bits-pilani.ac.in>
2022-10-02 04:52:32 +0530

    Time: 2 ms (70.24%), Space: 41.9 MB (77.13%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,12 +1,13 @@
 class Solution {
     Integer t[];
+    char arr[];
     public int numDecodings(String s) {
         t=new Integer[s.length()];
-        char arr[]=s.toCharArray();
-        return value(s.length(),0,arr);
+        arr=s.toCharArray();
+        return value(s.length(),0);
     }
     
-    public int value(int n, int i, char arr[]){
+    public int value(int n, int i){
         if(i==n)
             return 1;
         if(arr[i]=='0')
@@ -16,9 +17,9 @@ class Solution {
         
         int sum=0;
         
-        sum+=value(n,i+1,arr);
+        sum+=value(n,i+1);
         if(i+1<n && ((arr[i]-'0')*10 + (arr[i+1]-'0')) <= 26)
-           sum+=value(n,i+2,arr);
+           sum+=value(n,i+2);
            
            return t[i]=sum;
     }
